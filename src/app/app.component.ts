71383ef50fdd1472e30deaac90ecafe592e99f7e
Marccudi <marccudi@hotmail.es>
2022-09-09 21:28:23 +0200

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Task1';
+  nombre="a";
+  cif="a";
+  direccion="a";
+  grupo=1;
+
+  clientes = [
+    {nombre:"Marc", cif:"46779864A", direccion:"C/20 Nº94 bajos", grupo:1},
+    {nombre:"Manolo", cif:"46779257A", direccion:"C/21 Nº94 bajos", grupo:2}
+  ]
+  constructor(){
+  }
+  anadir(nombres:string,cif:string, direccion:string, grupo:string){
+    this.nombre=this.nombre
+    this.clientes[this.clientes.length]={nombre:nombres, cif:cif, direccion:direccion, grupo:parseInt(grupo)};
+  }
 }
